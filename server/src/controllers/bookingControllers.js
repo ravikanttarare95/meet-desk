cc2e554c26f6ae8003672e58c03384bd78e9fc76
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-12-24 14:34:45 +0530

    createBooking

--- server/src/controllers/bookingControllers.js
+++ server/src/controllers/bookingControllers.js
@@ -3,10 +3,10 @@ import Booking from "./../models/Booking.js";
 
 const getAvailableSlots = async (req, res) => {
   try {
-    const { adminId, date } = req.query;
+    const { userId, date } = req.query;
 
     const availability = await Availability.findOne({
-      adminId,
+      userId,
       date,
       isBlocked: false,
     });
@@ -22,7 +22,7 @@ const getAvailableSlots = async (req, res) => {
     );
 
     const bookings = await Booking.find({
-      adminId,
+      userId,
       date,
       status: "BOOKED",
     });
@@ -40,5 +40,63 @@ const getAvailableSlots = async (req, res) => {
   }
 };
 
-const createBooking = async (req, res) => {};
+const createBooking = async (req, res) => {
+  try {
+    const { userId, date, startTime, endTime, userName, userEmail, purpose } =
+      req.body;
+
+    const availability = await Availability.findOne({
+      userId,
+      date,
+      isBlocked: false,
+    });
+
+    if (!availability) {
+      return res.status(400).json({ message: "No availability" });
+    }
+
+    const slots = generateSlots(
+      availability.startTime,
+      availability.endTime,
+      availability.slotDuration
+    );
+
+    const isValidSlot = slots.some(
+      (slot) => slot.start === startTime && slot.end === endTime
+    );
+
+    if (!isValidSlot) {
+      return res.status(400).json({ message: "Invalid slot" });
+    }
+
+    const alreadyBooked = await Booking.findOne({
+      userId,
+      date,
+      startTime,
+      endTime,
+      status: "BOOKED",
+    });
+
+    if (alreadyBooked) {
+      return res.status(409).json({ message: "Slot already booked" });
+    }
+
+    const booking = await Booking.create({
+      userId,
+      date,
+      startTime,
+      endTime,
+      userName,
+      userEmail,
+      purpose,
+    });
+
+    res.status(201).json({
+      success: true,
+      booking,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
 export { getAvailableSlots, createBooking };
