99c11dbbed4101f9d575744ebdc14821cdd8f160
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-12-24 12:42:35 +0530

    createAvailability controller

--- server/src/controllers/availabilityControllers.js
+++ server/src/controllers/availabilityControllers.js
@@ -1,4 +1,41 @@
-const createAvailability = async (req, res) => {};
+import Availability from "./../models/Availability.js";
+
+const createAvailability = async (req, res) => {
+  try {
+    const { date, startTime, endTime, slotDuration } = req.body;
+    const userId = req.user.id;
+
+    if (!date || !startTime || !endTime || !slotDuration) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const exists = await Availability.findOne({
+      userId,
+      date,
+    });
+
+    if (exists) {
+      return res.status(409).json({
+        message: "Availability already exists for this date",
+      });
+    }
+
+    const availability = await Availability.create({
+      userId,
+      date,
+      startTime,
+      endTime,
+      slotDuration,
+    });
+
+    res.status(201).json({
+      success: true,
+      availability,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
 const getAvailability = async (req, res) => {};
 
--- server/src/models/Availability.js
+++ server/src/models/Availability.js
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 const availabilitySchema = new Schema(
   {
-    adminId: {
+    userId: {
       type: Schema.Types.ObjectId,
       ref: "User",
       required: true,
