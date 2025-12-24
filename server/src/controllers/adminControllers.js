f10d2b60c1305df9fd0a4dd99f1c891448da9d6c
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-12-24 16:00:35 +0530

    cancelBooking

--- server/src/controllers/adminControllers.js
+++ server/src/controllers/adminControllers.js
@@ -15,4 +15,27 @@ const getAdminBookings = async (req, res) => {
   }
 };
 
-export { getAdminBookings };
+const cancelBooking = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { id } = req.params;
+
+    const booking = await Booking.findOne({
+      _id: id,
+      userId,
+    });
+
+    if (!booking) {
+      return res.status(404).json({ message: "Booking not found" });
+    }
+
+    booking.status = "CANCELLED";
+    await booking.save();
+
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export { getAdminBookings, cancelBooking };
