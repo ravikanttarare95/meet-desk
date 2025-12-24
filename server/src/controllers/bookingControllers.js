e778a85a216941669a66ca0e5cd051f6e5cc75ff
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-12-24 14:29:35 +0530

    getAvailableSlots

--- server/src/controllers/bookingControllers.js
+++ server/src/controllers/bookingControllers.js
@@ -1,7 +1,44 @@
 import Availability from "./../models/Availability.js";
 import Booking from "./../models/Booking.js";
+
+const getAvailableSlots = async (req, res) => {
+  try {
+    const { adminId, date } = req.query;
+
+    const availability = await Availability.findOne({
+      adminId,
+      date,
+      isBlocked: false,
+    });
+
+    if (!availability) {
+      return res.json([]);
+    }
+
+    const allSlots = generateSlots(
+      availability.startTime,
+      availability.endTime,
+      availability.slotDuration
+    );
+
+    const bookings = await Booking.find({
+      adminId,
+      date,
+      status: "BOOKED",
+    });
+
+    const availableSlots = allSlots.filter((slot) => {
+      return !bookings.some(
+        (booking) =>
+          booking.startTime === slot.start && booking.endTime === slot.end
+      );
+    });
+
+    res.json(availableSlots);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 const createBooking = async (req, res) => {};
-
-const getAvailableSlots = async (req, res) => {};
-
-export { createBooking, getAvailableSlots };
+export { getAvailableSlots, createBooking };
