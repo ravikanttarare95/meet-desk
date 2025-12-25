ce2f9644e7aa2eb4417ab0f0b62ec4fb2381d7f0
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-12-25 10:52:22 +0530

    NOT FOUND page

--- client/src/views/NotFound.jsx
+++ client/src/views/NotFound.jsx
@@ -1,14 +1,37 @@
-import { Link } from "react-router";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import Button from "../components/form_components/Button";
 
 function NotFound() {
-  return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold text-red-600">404</h1>
-      <p className="text-gray-600 mt-2">Page not found</p>
+  const navigate = useNavigate();
 
-      <Link to="/dashboard" className="mt-4 text-violet-600 underline">
-        Go to Dashboard
-      </Link>
+  const [user] = useState(JSON.parse(localStorage.getItem("loggedInUser")));
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-violet-50 to-gray-100 px-4">
+      <div className="bg-white border border-gray-200 rounded-2xl shadow-lg p-10 max-w-md w-full text-center space-y-6">
+        <h1 className="text-7xl font-extrabold text-rose-500 tracking-widest">
+          404
+        </h1>
+
+        <div className="space-y-2">
+          <p className="text-xl font-semibold text-gray-800">Page Not Found</p>
+          <p className="text-gray-500 text-sm">
+            Sorry, the page you are looking for doesn’t exist or has been moved.
+          </p>
+        </div>
+        <Button
+          btnTitle={"Go to Booking Page"}
+          btnVariant={"primary"}
+          onBtnClick={() => {
+            if (user?.id) {
+              navigate(`/book/${user.id}`);
+            } else {
+              navigate("/");
+            }
+          }}
+        />
+      </div>
     </div>
   );
 }
